02182a404644bbba64ef07dd6ea9fca0cfccc733
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-21 22:33:51 +0900

    Chapter6_3@定数

---
+++ src/practice/Chapter6_3.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Chapter6_3 {
+	public static void main(String[] args) {
+		//	定数の初期化（宣言、代入）
+		final int num1 = 10000;
+		
+		System.out.println("num1は定数で、値は" + num1 + "です。");
+		
+		//	定数の再代入（Error）
+		//	num1 = 15000;
+	}
+}
