d96d272d981d4444710142543c9a4981074d22a7
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-21 23:59:06 +0900

    Chapter7_3@データ型（boolean型）

---
+++ src/practice/Chapter7_3.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Chapter7_3 {
+	public static void main(String[] args) {
+		boolean var1 = true;
+		boolean var2 = false;
+		boolean var3 = (6 == 8);
+		boolean var4 = (10 > 1);
+		
+		System.out.println("var1：" + var1);
+		System.out.println("var2：" + var2);
+		System.out.println("var3：" + var3);
+		System.out.println("var4：" + var4);
+	}
+}
