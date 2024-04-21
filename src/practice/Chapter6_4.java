cbf8fbb0f0bcff25210bff7c1f0585caa2b7a164
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-21 22:43:01 +0900

    Chapter6_4@varによる変数宣言

---
+++ src/practice/Chapter6_4.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Chapter6_4 {
+	public static void main(String args[]) {
+		//	varによる変数宣言
+		var num1 = 100;
+		var num2 = 1.1;
+		var num3 = true;
+		
+		System.out.println("num1：" + num1);
+		System.out.println("num2：" + num2);
+		System.out.println("num3：" + num3);
+	}
+}
