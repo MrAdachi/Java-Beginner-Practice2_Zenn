df5952e7c742d7a5925189b25a95b07cccf3cbcb
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-21 22:07:21 +0900

    Chapter6_1@変数の宣言・代入・参照

---
+++ src/practice/Chapter6_1.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class Chapter6_1 {
+	public static void main(String[] args) {
+		//	変数の宣言
+		int var1;
+		
+		//	変数の代入
+		var1 = 100;
+		
+		//	変数の初期化（宣言と代入）
+		int var2 = 200;
+		
+		//	変数の参照と出力
+		System.out.println("var1：" + var1);
+		System.out.println("var2：" + var2);
+		
+		//	変数の複数宣言
+		int var3, var4;
+		var3 = 300;
+		var4 = 400;
+		
+		System.out.println("var3：" + var3);
+		System.out.println("var4：" + var4);
+	}
+}
