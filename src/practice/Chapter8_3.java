c28ad8ac9c6b4a5321bf34464109902b85cd3d72
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-23 01:13:28 +0900

    Chapter8_3@コマンドライン引数

---
+++ src/practice/Chapter8_3.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Chapter8_3 {
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+	}
+}
