ba233aa71467c686c2ac8c2d0d903f44da8c0b52
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-23 20:19:28 +0900

    Chapter9_4@複合演算子

---
+++ src/practice/Chapter9_4.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Chapter9_4 {
+	public static void main(String[] args) {
+		// 複合演算子
+		int x = 10;
+		int y = 2;
+		
+		System.out.println(x += 10);
+		System.out.println(y *= 5);
+	}
+}
