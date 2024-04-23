c9231bfd00f9545e85e8dce6aca879640a1f0f3d
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-23 18:24:36 +0900

    Chapter9_2@関係演算子

---
+++ src/practice/Chapter9_2.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Chapter9_2 {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 2;
+		int z = 10;
+		
+		// ※ "=" は必ず右側にくる
+		System.out.println(x > y); //xはyより大きいか
+		System.out.println(x < y); //xはyより小さいか
+		System.out.println(x >= z); //xはz以上か
+		System.out.println(x == z); //xとzは等しいか
+		System.out.println(x != z); //xとzは異なるか
+	}
+}
