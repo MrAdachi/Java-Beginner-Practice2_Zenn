31963c71ae9f71b7559031c1c39c5b85e623e8b6
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-23 23:47:52 +0900

    Chapter10_3@if～elseif文

---
+++ src/practice/Chapter10_3.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Chapter10_3 {
+	public static void main(String[] args) {
+		int age = 18;
+		
+		if(age == 20) {
+			System.out.println("二十歳です。");
+		} else if(age >= 18) {
+			System.out.println("二十歳まで後少しです。");
+		} else {
+			System.out.println("二十歳ではありません。");
+		}
+	}
+}
