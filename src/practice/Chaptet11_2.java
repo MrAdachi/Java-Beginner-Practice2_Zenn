7c294242e6a5882f3ee8236d08360390222bce1b
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-24 02:30:20 +0900

    Chapter11_2@do～while文

---
+++ src/practice/Chaptet11_2.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Chaptet11_2 {
+	public static void main(String[] args) {
+		int i = 15;
+		
+		// while文とdo while文の違い
+		// while内の条件式を評価 → false（1回目） → 処理文は実行されない
+		while(i != 15 && i == 14) {
+			System.out.println(i);
+		}
+		
+		// doの処理分を実行（1回目） → 中身が実行 → while内の条件式を評価
+		do {
+			System.out.println(i);
+			i--;
+		} while(i != 15 && i == 14);
+	}
+}
