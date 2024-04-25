7ec55902076e9a0364fb43fa2867958efe8a565f
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-25 16:18:24 +0900

    Chapter13_2@メンバ間のアクセス（staticメソッドからのアクセス）

---
+++ src/practice/Chapter13_2.java
@@ -0,0 +1,34 @@
+package practice;
+
+public class Chapter13_2 {
+	
+	String instanceVal = "instance変数";
+	static String staticVal = "static変数";
+	
+	void instanceMethod() {
+		System.out.println(instanceVal);
+	}
+	
+	static void staticMethod() {
+		System.out.println(staticVal);
+	}
+	
+	public static void main(String[] args) {
+		// インスタンスメンバへのアクセス
+		Chapter13_2 t = new Chapter13_2();
+		System.out.println(t.instanceVal);
+		// System.out.println(instanceVal); エラー発生（staticメンバ → instanceメンバへの直接アクセス不可）
+		
+		t.instanceMethod();
+		// instanceMethod();  エラー発生（staticメンバ → instanceメンバへの直接アクセス不可）
+		
+		// staticメンバへのアクセス
+		System.out.println(staticVal);
+		System.out.println(Chapter13_2.staticVal);
+		System.out.println(t.staticVal);
+		
+		staticMethod();
+		Chapter13_2.staticMethod();
+		t.staticMethod();
+	}
+}
