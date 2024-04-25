26488e83000a625675bfaeba24d4290bcb1668a6
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-25 23:36:33 +0900

    Chapter14_1@アクセス修飾子

---
+++ src/practice/Chapter14_1.java
@@ -0,0 +1,25 @@
+package practice;
+
+class Test14_1 {
+	private int num; // インスタンス変数にprivate
+	
+	public Test14_1(int i) {
+		num = i;
+	} // コンストラクタにpublic
+	
+	public int getnum() {
+		return num;
+	} // メソッドにpublic
+}
+
+public class Chapter14_1 {
+	public static void main(String[] args) {
+		Test14_1 t = new Test14_1(100);
+		
+		/* System.out.println(t.num);
+		   エラー（privateのアクセス修飾子は同一クラスでのみ使用可能であるため）
+		 */
+		System.out.println(t.getnum());
+	}
+
+}
