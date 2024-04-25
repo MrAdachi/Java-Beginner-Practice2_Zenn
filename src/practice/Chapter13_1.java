f88957aee934317f26221898178a9f469fd3baf7
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-25 15:59:49 +0900

    Chapter13_1@メンバ間のアクセス（同クラス内）

---
+++ src/practice/Chapter13_1.java
@@ -0,0 +1,46 @@
+package practice;
+
+class Test13_1 {
+	String instanceVal = "instance変数";
+	static String staticVal = "static変数";
+	
+	void instancemethod() {
+		System.out.println(instanceVal);
+	}
+	
+	static void staticmethod() {
+		System.out.println(staticVal);
+	}
+	
+	void instancemethodB() {
+		System.out.println(staticVal);
+	}
+	
+	/* static void staticmethodB() {
+		System.out.println(instanceVal);
+	}
+	staticメンバからインスタンスメンバへのアクセス → エラー
+	*/
+	
+	// Test t = new Test(); ここに定義したらtもインスタンスメンバ
+	
+	static void staticMethodB() {
+		Test13_1 t = new Test13_1(); // ローカル変数として定義しないとNG！
+		t.instancemethod();
+	}
+}
+
+public class Chapter13_1 {
+	public static void main(String[] args) {
+		Test13_1 t = new Test13_1();
+		
+		System.out.println(t.instanceVal);
+		System.out.println(t.staticVal);
+		System.out.println(Test13_1.staticVal);
+		
+		t.instancemethod();
+		t.staticmethod();
+		t.instancemethodB();
+		t.staticMethodB();
+	}
+}
