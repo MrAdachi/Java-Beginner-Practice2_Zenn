f7bf64f4e1d57d9769ed7fd79ca7be08bfc39ea4
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-26 22:49:21 +0900

    Chapter21_7@コンストラクタの呼び出し（superの指定なし）

---
+++ src/practice/Chapter21_7.java
@@ -0,0 +1,30 @@
+package practice;
+
+// スーパークラス
+class Super21_7 {
+	public Super21_7() {
+		System.out.println("Super21_7");
+	}
+	
+	public Super21_7(int i) {
+		System.out.println("SuperA int：" + i);
+	}
+}
+
+// サブクラス
+class Sub21_7 {
+	public Sub21_7() {
+		System.out.println("Sub21_7");
+	}
+	
+	public Sub21_7(int i) {
+		System.out.println("SubA int:" + i);
+	}
+}
+
+public class Chapter21_7 {
+	public static void main(String[] args) {
+		Sub21_7 s1 = new Sub21_7();
+		Sub21_7 s2 = new Sub21_7(500);
+	}
+}
